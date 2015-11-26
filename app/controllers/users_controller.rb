b90d8ff954e6732f4783c36f70e13197e14a1887
tsalmon <th_s@hotmail.fr>
2015-11-26 20:24:56 +0100

    add constant variable where is launched the id of the leader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
 
   # GET /users/hierarchy
   def hierarchy
+
   end
 
   # POST /users
---
+++ config/initializers/constants_hierarchy.rb
@@ -0,0 +1,2 @@
+# :user => { :id } of leader (parent of all users)
+LEADER_ID = 1
