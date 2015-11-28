329e9f365cf3feb8d785c0e0934b39783e33cf47
tsalmon <th_s@hotmail.fr>
2015-11-28 18:07:24 +0100

    Hierachy CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,39 @@
 module ApplicationHelper
 
   def hierarchy_toString(lead)
-	str = "<a href=\"#{ user_path(lead.id) }\">#{lead.name + " " + lead.surname}</a></li>"
-	if(lead.children.size == 0)
-		('<li class="last">' + str).html_safe
+	str = lead.name + " " + lead.surname
+	unless lead.parent
+		str = '<li class="root">' + "#{ link_to str , user_path(lead.id) }" + '</li>'
+		lead.children.each do |c|
+			str += "<li>" + hierarchy_toString(c) + "</li>"
+		end
 	else
-		str += '<ul class="tree">'
+		str = "<li>#{ link_to str, user_path(lead.id) }"
+		if(lead.children.size == 0)
+			(str).html_safe
+		else
+			str += '<ul>'
+			lead.children.each do |c|
+				str += hierarchy_toString(c)
+			end
+			str += "</ul></li>"
+			str.html_safe
+		end
+	end
+	str.html_safe
+  end
+
+  def other
+	str = "<li><a href=\"#{ user_path(lead.id) }\">#{lead.name + " " + lead.surname}</a>"
+	if(lead.children.size == 0)
+		(str + "</li>").html_safe
+	else
+		str += '<ul>'
 		lead.children.each do |c|
 			str += hierarchy_toString(c)
 		end
-		str += "</ul>"
-		('<li>' + str).html_safe
+		str += "</ul></li>"
+		str.html_safe
 	end
   end
 end
